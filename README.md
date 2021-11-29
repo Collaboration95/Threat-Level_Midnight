b327785ad078b8c9ca86e997385e115279702317
= <=>
2021-11-30 02:34:30 +0800

    Functional + Bug Fixes
    Fixed another bug in the exiting function , Added some more functionality to the admin controls , made some to-do changes to the readme file , Got a basic idea on how to implement highscore list and user score list. Feeling my thinking dullen , will stop for now

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 Stuff to do 
 Test out the queston ideas ( dict into files)
 Plot out stuff needed for an admin user 
-Admin user stuff
+
+
+
+--Admin user stuff--
 2 functions
 Sort_By_Highscores
 Sort_By_Players
-Reset_Scores
+
+--General Coding--
+Loop the edge cases/invalid options ( try to use same function )
 Try to make text based ui slow and feel old
--- program.py
+++ program.py
@@ -1,16 +1,17 @@
 import time , os.path
-def Show_And_Get_Options():
+def Show_And_Get_Options(Current_Options:dict)->int:
+    # This functions accepts a dictionary of the available options and returns the integer (Key) of the option
     # This shows the different options and returns the "Chosen one"
     while True:
         # This loops until the user selects a valid option or throws his system off a cliff
-        for key in options:
-            print(key , options[key])
+        for key in Current_Options:
+            print("{} : {}".format(key , Current_Options[key]))
         # For loop to loop through the dictionary and display options
         A = int(input("Please choose an option") )
         # Takes an input for option
         
         # checks if option is valid
-        if A in options:
+        if A in Current_Options:
             return A
         else:
             print("Well looks like you are one of those dumb users we have to be on the lookout for\n ")
@@ -26,7 +27,7 @@ def Read_Scores():
                 for i in f:
                     print(i)
             f.close()
-
+    
 
     # If file does not exit , this else block creates a file and writes an empty message in it
     else:
@@ -59,25 +60,53 @@ def Admin_Controls():
     
     # Notes by the programmer to the programmer:
     # Create a function to rank players by their highscores
-    # Create another fucntion to output all the scores of a particular function
+    # Create another fucntion to output all the scores of a particular player
+
+    # displays the entire dictionary
+    # Getting the chosen option by passing the Admin Option dictionary to the Show and get options function
+    Option = Show_And_Get_Options(Admin_options)
+    # Calling the Routing Function using the Option given by Show and get options function
+    Routing_function_Admin_Controls(Option)
+
+def Reset_Scores() ->None:
+    # This function can be called by Selecting Admin Controls > Selecting Reset Scores
+    # This function is to reset scores of the game
+    # The code opens the text file in which all the data is stored and writes an empty character instead of it 
+    # Hence Clearing it
+    f = open("users.txt","w")
+    f.write("")
+    f.close()
+    print("All data has been reset , have a nonconsequential day")
+    return None
+    
 
 
-
-
-def Routing_function(N:int)->None:
+def Routing_function_Admin_Controls(N:int)->None:
     # This function is to route the functions and the correct options 
     if N==1:
         pass
     elif N==2:
+        pass
+    elif N==3:
+        Reset_Scores()
+    else:
+        print("I am to lazy to code edge cases right now")
+
+def Routing_function(N:int)->None:
+    # This function is to route the functions and the correct options 
+    if N==1:
+        print("Code under construction")
+    elif N==2:
         Read_Scores()
     elif N==3:
-        pass
+        Admin_Controls()
     else:
-        if(input("Enter anything if you do not want to exit this godawful game")==""):
-            exit("Reality is often disappointing")
-        else:
+        if(input("Enter anything if you do not want to exit this godawful game")):
             # This line rescursively calls the routing function with the output of the Show_And_Get_Option function
-            Routing_function(Show_And_Get_Options)
+            Routing_function(Show_And_Get_Options(options))
+        else:
+            # This exits the program , thats it 
+            exit("Reality is often disappointing")
 
 def Main():
     # This is the first function that gets called , it provides the options
@@ -88,7 +117,7 @@ def Main():
     
     
     # Gets option input
-    N = Show_And_Get_Options()
+    N = Show_And_Get_Options(options)
 
     # Calls the routing function to get routed to the appropriate function
     Routing_function(N)
@@ -96,7 +125,7 @@ def Main():
 options = dict()
 options =   {1:"New Games",2:"See Scores",3:"Admin (Top-secrety stuff)",4:"Crawl under a rock for eternity"}
 Password = 3200
-
+Admin_options =   {1:"Sort By High scores",2:"Sort By Player Performance",3:"Reset Scores"}
 # This calls the main function indefinitely 
 while True:
     Main()
